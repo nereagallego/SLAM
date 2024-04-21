df6224be69d1ba679f9422dc75965a0c4ab091fa
Nerea <801950@unizar.es>
2024-04-21 09:38:33 -0700

    Add script to calculate mean and standard deviation of processed frames

---
+++ Mini-SLAM-lab4/mean_std_frames_processed.sh
@@ -0,0 +1,23 @@
+# Get as input the dataset name
+dataset_name=$1
+echo "Dataset name: $dataset_name"
+total_frames=0
+std_dev=0
+for i in $(seq 1 10)
+do
+    # Count the lines in the file
+    num_lines=$(wc -l < trajectory_$dataset_name\_$i.txt)
+    total_frames=$((total_frames + num_lines))
+done
+mean=$(echo "scale=2; $total_frames / 10" | bc)
+
+for i in $(seq 1 10)
+do
+    # Count the lines in the file
+    num_lines=$(wc -l < trajectory_$dataset_name\_$i.txt)
+    std_dev=$(echo "$std_dev + ($num_lines - $mean) * ($num_lines - $mean)" | bc -l)
+done
+std_dev=$(echo "scale=2; $std_dev / 10" | bc -l)
+std_dev=$(echo "scale=2; sqrt($std_dev)" | bc -l)
+echo "Mean: $mean"
+echo "Standard deviation: $std_dev"
