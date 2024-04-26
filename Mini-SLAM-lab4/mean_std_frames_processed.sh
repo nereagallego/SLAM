5ad0d0c8f02a44c50e549390c083868bee6bc17e
cborjamoreno <cesarborjamoreno@gmail.com>
2024-04-26 14:06:12 +0200

    trajectories

--- Mini-SLAM-lab4/mean_std_frames_processed.sh
+++ Mini-SLAM-lab4/mean_std_frames_processed.sh
@@ -3,21 +3,21 @@ dataset_name=$1
 echo "Dataset name: $dataset_name"
 total_frames=0
 std_dev=0
-for i in $(seq 1 10)
+for i in $(seq 1 5)
 do
     # Count the lines in the file
     num_lines=$(wc -l < trajectory_$dataset_name\_$i.txt)
     total_frames=$((total_frames + num_lines))
 done
-mean=$(echo "scale=2; $total_frames / 10" | bc)
+mean=$(echo "scale=2; $total_frames / 5" | bc)
 
-for i in $(seq 1 10)
+for i in $(seq 1 5)
 do
     # Count the lines in the file
     num_lines=$(wc -l < trajectory_$dataset_name\_$i.txt)
     std_dev=$(echo "$std_dev + ($num_lines - $mean) * ($num_lines - $mean)" | bc -l)
 done
-std_dev=$(echo "scale=2; $std_dev / 10" | bc -l)
+std_dev=$(echo "scale=2; $std_dev / 5" | bc -l)
 std_dev=$(echo "scale=2; sqrt($std_dev)" | bc -l)
 echo "Mean: $mean"
 echo "Standard deviation: $std_dev"
--- Mini-SLAM-lab4/script.sh
+++ Mini-SLAM-lab4/script.sh
@@ -1,8 +1,9 @@
-dataset='MH01'
+dataset='fr1_floor'
 task='task2'
-for i in $(seq 1 10)
+for i in $(seq 1 5)
 do
+    echo "Iteration $i"
     mkdir -p trajectories/trajectory_$dataset\_$task
-    ./Apps/mono_euroc /home/nerea/SLAM/datasets/$dataset Apps/EuRoC_TimeStamps/$dataset.txt > test.txt
+    ./Apps/mono_tumrgbd /home/cesar/MRGCV/SLAM/datasets/$dataset > test.txt
     mv trajectory.txt trajectories/trajectory_$dataset\_$task/trajectory_$dataset\_$task\_$i.txt
 done
